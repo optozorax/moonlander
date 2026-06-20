47984b95bbdb68c4feccf23fa4a3c4992db870ca
Ilya Sheprut <optozorax@gmail.com>
2026-06-20 13:40:26 +0300

    fix reset button

--- keymap.c
+++ keymap.c
@@ -360,7 +360,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, // LEFT THUMB KEYS
 
     // RIGHT HALF
-    _______, _______, _______, _______, _______, _______, RESET,
+    _______, _______, _______, _______, _______, _______, QK_BOOT,
     _______, _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______,
     _______, _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   _______,
              _______, KC_F9,   KC_F8,   KC_F7,   KC_F12,  _______,
@@ -487,7 +487,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_RCTL, KC_RSFT, _______, // LEFT THUMB KEYS
 
     // RIGHT HALF
-    _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, RESET,
+    _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, QK_BOOT,
     _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
     _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PENT, _______,
              _______, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______,
